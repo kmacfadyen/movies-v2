d9d2fdad12939ab17cf13e53c3d9e507e3e1ed65
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-01-22 23:54:16 -0500

    api test

--- movies-v2/src/App.jsx
+++ movies-v2/src/App.jsx
@@ -1,21 +1,26 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 
+// Api key: f1a20cee
+
+const API_URL = 'http://www.omdbapi.com?apikey=f1a20cee';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
 
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+ 
   useEffect(() => {
-    setCounter(100);
+    searchMovies('Harry Potter');
   }, []);
 
   return (
-    <div className='App'>
-      <button onClick={() => setCounter(counter - 1)} >-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter(counter + 1)} >+</button>
-
-    </div>
+    <h1>App</h1>
   )
 }
 
